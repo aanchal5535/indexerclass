d0ad90b27d8072e69a1a27bf6b9f5ca4ba2539a2
aanchal-subedi <aanchalsubedi005@gmail.com>
2023-04-27 13:01:02 +0545

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class program
+{
+    class IndexerClass
+    {
+        private string[] names = new string[10];
+        public string this[int i]
+        {
+            get
+            {
+                return names[i];
+            }
+            set
+            {
+                names[i] = value;
+            }
+        }
+    }
+    static void Main(string[] args)
+    {
+        IndexerClass Team = new IndexerClass();
+        Team[0] = "Rocky";
+        Team[1] = "teena";
+        Team[2] = "ana";
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(Team[i]);
+        }
+        Console.ReadKey();
+    }
+}
